8f486e801d6ae2fa91625d53d6c35ffbafae9fc8
Marek Urbanek <marek.urbi@gmail.com>
2019-05-31 18:25:34 +0200

    Added new task component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskListComponent,
-    TaskComponent
+    TaskComponent,
+    NewTaskComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/new-task/new-task.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-task',
+  templateUrl: './new-task.component.html'
+})
+export class NewTaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
